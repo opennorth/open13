ee9a4440aace3bcd58b85ac88996ef6cd5286ccf
James Turk <james.p.turk@gmail.com>
2012-11-06 13:58:43 -0500

    add basic billy_settings.py

---
+++ billy_settings.py
@@ -0,0 +1,11 @@
+import os
+
+SCRAPER_PATHS = [os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                              'scrapers')]
+MONGO_HOST = 'localhost'
+MONGO_PORT = 27017
+MONGO_DATABASE = 'open13'
+
+# PARTY_DETAILS
+
+LEVEL_FIELD = 'jurisdiction'
