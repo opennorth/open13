039f9b05b0e304eabcdaf21c068072abf60e3694
James Turk <james.p.turk@gmail.com>
2012-11-06 16:21:36 -0500

    preliminary ON bill scraper

---
+++ scrapers/on/bills.py
@@ -0,0 +1,55 @@
+from datetime import datetime
+import re
+import lxml.html
+from billy.scrape.bills import BillScraper, Bill
+
+def _clean_spaces(s):
+    return re.sub('\s+', ' ', s, flags=re.U).strip()
+
+class ONBillScraper(BillScraper):
+    jurisdiction = 'on'
+
+    def scrape(self, session, chambers):
+        url = 'http://www.ontla.on.ca/web/bills/bills_all.do?locale=en&parlSessionID=%s' % session
+        html = self.urlopen(url)
+        doc = lxml.html.fromstring(html)
+        doc.make_links_absolute(url)
+
+        for row in doc.xpath('//table/tr'):
+            id, title_td, sponsor = row.xpath('td')
+            bill_id = id.text_content().strip()
+            title = title_td.text_content().strip()
+            # pull sponsor off different page
+            bill = Bill(session, 'lower', bill_id, title)
+            # skip to detail page
+            detail_url = title_td.xpath('a/@href')[0] + "&detailPage=bills_detail_status"
+            bill.add_source(url)
+            bill.add_source(detail_url)
+
+            # get actions & sponsors
+            self.scrape_details(bill, detail_url)
+
+            self.save_bill(bill)
+
+    def scrape_details(self, bill, detail_url):
+        data = self.urlopen(detail_url)
+        doc = lxml.html.fromstring(data)
+
+        # sponsors
+        for sp in doc.xpath('//span[@class="pSponsor"]/text()'):
+            bill.add_sponsor('primary', sp)
+        for sp in doc.xpath('//span[@class="sSponsor"]/text()'):
+            bill.add_sponsor('cosponsor', sp)
+
+        for row in doc.xpath('//table//tr')[1:]:
+            date, stage, activity, committee = row.xpath('td/text()')
+            date = datetime.strptime(_clean_spaces(date), "%B %d, %Y")
+            stage = _clean_spaces(stage)
+            activity = _clean_spaces(activity)
+            committee = _clean_spaces(committee)
+            # action prefixed with stage if present
+            action = '%s - %s' % (stage, activity) if stage else activity
+            # default to lower, use committee if present
+            actor = committee if committee else 'lower'
+            bill.add_action(actor, action, date)
+
